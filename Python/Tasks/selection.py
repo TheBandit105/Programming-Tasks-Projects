2e388a96feadd229688dc643736387b5c724ec7d
Shavin Croos <87244418+TheBandit105@users.noreply.github.com>
2021-07-12 00:27:37 +0100

    Added comments to selection.py
    Added comments to selection.py to explain how the algorithm worked in the code.

--- Python/Tasks/selection.py
+++ Python/Tasks/selection.py
@@ -1,7 +1,15 @@
 def selection_sort(array):
+
+    # This outer loop will start to store each element's value
+    # and current index number from the input, which is a
+    # list reference.
+    
     for index in range(0, len(array) - 1):
         value = array[index]
         current = index
+
+        # Inner loop executes main algorithm by where the smallest unsorted
+        # value is swapped with the first unsorted value each time.
 
         for element in range(index + 1, len(array)):
             if array[element] < array[current]:
@@ -11,8 +19,13 @@ def selection_sort(array):
         array[current] = value
         print('\tResolving element [', index, '] to', array)
 
+# Declared list as array and display the unsorted array.
+
 array = [5, 3, 1, 2, 6, 4]
 print('Selection Sort...\nArray :', array)
+
+# Call the function selection_sort and display the output of the function,
+# which is the sorted list.
 
 selection_sort(array)
 print('Array: ', array)
