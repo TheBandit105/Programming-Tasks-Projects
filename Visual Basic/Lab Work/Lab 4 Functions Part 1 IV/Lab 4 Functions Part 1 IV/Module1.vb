bd20cc505388c2357b9f2e31aec9aca3a405750c
Shavin Croos <87244418+TheBandit105@users.noreply.github.com>
2022-01-13 00:14:21 +0000

    Lab 4 work Part 3 and Lab 5 work Part 1
    Lab 5 involves functions as well, however, its incomplete at the moment. If anyone is a current computer science student doing or had done the foundation year at Brunel University, please send a copy of the original task sheet linked to this lab work. Thank you.

---
+++ Visual Basic/Lab Work/Lab 4 Functions Part 1 IV/Lab 4 Functions Part 1 IV/Module1.vb
@@ -0,0 +1,36 @@
+﻿Imports System.Math
+Imports System.Text
+Module Module1
+
+    Sub Main()
+        Dim x As Double, y As Double, z As Double
+        Dim a As String, b As String, c As String
+        Dim length As Integer, i As Integer
+
+        x = 123.456
+        y = 17 / 3
+        z = Sqrt(2)
+        a = "Hello"
+        b = "World"
+
+        c = ""
+        length = Len(a)
+        For i = length To 1 Step -1
+            c = c + Mid(a, i, 1)
+        Next
+        c = c + " " + b
+
+        Console.WriteLine(c)
+
+        Console.WriteLine(" ")
+
+        Console.WriteLine(Replace(c, "ll", "ppp"))
+
+        Console.WriteLine(" ")
+
+
+
+
+    End Sub
+
+End Module
---
+++ Visual Basic/Lab Work/Lab 5 Functions II/Lab 5 Functions II/Module1.vb
@@ -0,0 +1,54 @@
+﻿Module Module1
+
+    Sub Main()
+        task521()
+        task522()
+        task523()
+        task524()
+        task525()
+
+    End Sub
+
+    Sub task521()
+        Dim x As Integer, ftl As Integer
+
+        x = 13
+
+        ftl = Factorial(x)
+
+        Console.WriteLine("The factorial of" + Str(x) + " is" + Str(Factorial(x)))
+
+    End Sub
+
+    Function Factorial(ByVal y As Byte) As Long
+        Dim I As Long
+        If y <= 0 Then
+            Exit Function
+        Else
+            Factorial = 1
+            For I = 1 To y
+                Factorial = Factorial * I
+            Next I
+        End If
+
+        Return I
+
+    End Function
+
+
+    Sub task522()
+
+    End Sub
+
+    Sub task523()
+
+    End Sub
+
+    Sub task524()
+
+    End Sub
+
+    Sub task525()
+
+    End Sub
+End Module
