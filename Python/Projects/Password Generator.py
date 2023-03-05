727dd36a0f91b4733f60aaa90f337b848ffc7f01
Shavin Croos <t.s.croos@student.reading.ac.uk>
2023-03-05 21:08:39 +0000

    Added Some Comments

--- Python/Projects/Password Generator.py
+++ Python/Projects/Password Generator.py
@@ -1,17 +1,33 @@
 import random
 import string
 
+# The function generate_password calls in 3 parameters. The first, min_Length
+# is a compulsory parameter, which returns the password of that length. The other two
+# parameters, numbers and special characters, are optional, but have been set to
+# True as default unless specified otherwise.
+
 def generate_password(min_Length, numbers=True, special_characters=True):
     
-    # Obtains all the ascii letters (letters from a-z in both lower and upper caps), the numerical digits (0-9)
-    # and the special characters, which refers to the punctuation characters on the QWERTY keyboard.
+    # By importing the string module, all the ascii letters (letters from a-z in both lower and upper caps), 
+    # the numerical digits (0-9) and the special characters, which refers to the punctuation characters 
+    # on the QWERTY keyboard, can be accessed.
      
     letters = string.ascii_letters
     digits = string.digits
     special = string.punctuation
 
+    # Since letters are always going to be in the password,
+    # no matter what the password is, so the characters variable
+    # is going to contain the letters variable.
+
     characters = letters
 
+    # If either numbers or special_characters or both have been 
+    # set to True, this will allow the program to be able to
+    # access the numbers or special characters or both by
+    # concatenating them to the characters variable during the 
+    # password generation.
+    
     if numbers:
         characters += digits
     if special_characters:
