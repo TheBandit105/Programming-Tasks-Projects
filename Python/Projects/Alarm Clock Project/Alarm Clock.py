70828fe33ac0844ae256d7a501c74dbe02b9a942
Shavin Croos <t.s.croos@student.reading.ac.uk>
2023-04-09 00:29:59 +0100

    Updated Alarm Clock
    I have managed to add hours as an input to the alarm clock project. Next, will be figuring out how to add days, weeks, months and eventually years.

--- Python/Projects/Alarm Clock Project/Alarm Clock.py
+++ Python/Projects/Alarm Clock Project/Alarm Clock.py
@@ -1,28 +1,31 @@
 from playsound import playsound
 import time
 
+CLEAR = "\033[2J"
 CLEAR_AND_RETURN = "\033[H"
 
 def alarm(seconds):
     time_elapsed = 0
 
+    print(CLEAR)
     while time_elapsed < seconds:
         time.sleep(1)
         time_elapsed +=1
 
         time_left = seconds - time_elapsed
+        hours_left = time_left // 3600
         minutes_left = time_left // 60
         seconds_left = time_left % 60
 
-        print(f"{CLEAR_AND_RETURN}Alarm will sound in: {minutes_left:02d}:{seconds_left:02d}")
+        print(f"{CLEAR_AND_RETURN}Alarm will sound in: {hours_left:02d}:{minutes_left:02d}:{seconds_left:02d}")
 
     playsound('D:\\GITHUBREPOv2\\Programming-Tasks-Projects\\Python\\Projects\\Alarm Clock Project\\iPhone_Alarm_Original.mp3')
 
-
+hours = int(input("How many hours to wait: "))
 minutes = int(input("How many minutes to wait: "))
 seconds = int(input("How many seconds to wait: "))
 
-total_seconds = minutes * 60 + seconds
+total_seconds = hours * 3600 + minutes * 60 + seconds
 alarm(total_seconds)
 
 
