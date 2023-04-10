f18cce8fdf9e604b2eb194f8c0e32be1aad0d35a
Shavin Croos <t.s.croos@student.reading.ac.uk>
2023-04-10 21:50:18 +0100

    Updated Alarm Clock
    The alarm clock project can now input days as a user input. Plan to add the weeks, months and years. The total seconds left variable statement is there for reference to check that the alarm clock is working correctly for debugging purposes.

--- Python/Projects/Alarm Clock Project/Alarm Clock.py
+++ Python/Projects/Alarm Clock Project/Alarm Clock.py
@@ -13,19 +13,26 @@ def alarm(seconds):
         time_elapsed +=1
 
         time_left = seconds - time_elapsed
-        hours_left = time_left // 3600
+
+        days_left = int(time_left // 86400)
+        hours_left = int(time_left // 3600) % 24
         minutes_left = int(time_left // 60) % 60
         seconds_left = time_left % 60
-
-        print(f"{CLEAR_AND_RETURN}Alarm will sound in: {hours_left:02d}:{minutes_left:02d}:{seconds_left:02d}")
+       
+        print(f"{CLEAR_AND_RETURN}Alarm will sound in: {days_left:02d}:{hours_left:02d}:{minutes_left:02d}:{seconds_left:02d}")
+        print(f"\nTotal seconds left: {seconds}")
 
     playsound('D:\\GITHUBREPOv2\\Programming-Tasks-Projects\\Python\\Projects\\Alarm Clock Project\\iPhone_Alarm_Original.mp3')
 
+# years =
+# months =
+# weeks =
+days = int(input("How many days to wait: "))
 hours = int(input("How many hours to wait: "))
 minutes = int(input("How many minutes to wait: "))
 seconds = int(input("How many seconds to wait: "))
 
-total_seconds = hours * 3600 + minutes * 60 + seconds
+total_seconds = days * 86400 + hours * 3600 + minutes * 60 + seconds
 alarm(total_seconds)
 
 
