3f7a589126fdb8a4bd9dfd2d9ae5dbc5c612b567
TheBandit105 <shavinminecraft@gmail.com>
2025-03-21 19:36:02 +0000

    Finished Password Manager

--- Python/Projects/Password Manager.py
+++ Python/Projects/Password Manager.py
@@ -1,44 +1,59 @@
 from cryptography.fernet import Fernet
+import os
 
-'''
+# Function to generate and save a key (run once to generate 'key.key')
 def write_key():
     key = Fernet.generate_key()
     with open('key.key', 'wb') as key_file:
-        key_file.write(key)'''
+        key_file.write(key)
 
+# Function to load the key from the file
 def load_key():
-    file = open('key.key', 'rb')
-    key = file.read()
-    file.close()
+    if not os.path.exists('key.key'):
+        print("Key file not found! Generating a new key...")
+        write_key()
+    
+    with open('key.key', 'rb') as file:
+        key = file.read()
     return key
 
-master_pwd = input('What is the master password? ')
-key = load_key() + master_pwd.encode()
+# Load the encryption key
+key = load_key()
 fer = Fernet(key)
 
 def view():
+    if not os.path.exists('passwords.txt'):
+        print("No stored passwords found.")
+        return
+
     with open('passwords.txt', 'r') as f:
         for line in f.readlines():
-            data = line.rstrip()
-            user, passw = data.split('|')
-            print('User:', user, '| Password:', fer.decrypt(passw.encode()).decode())
-    
+            data = line.strip()
+            if '|' in data:
+                user, encrypted_passw = data.split('|')
+                try:
+                    decrypted_passw = fer.decrypt(encrypted_passw.encode()).decode()
+                    print(f'User: {user} | Password: {decrypted_passw}')
+                except Exception as e:
+                    print(f'Error decrypting password for {user}: {e}')
+
+
 def add():
     name = input('Account Name: ')
     pwd = input('Password: ')
+    encrypted_pwd = fer.encrypt(pwd.encode()).decode()
 
     with open('passwords.txt', 'a') as f:
-        f.write(name + '|' + fer.encrypt(pwd.encode()).decode() + '\n')
+        f.write(name + '|' + encrypted_pwd + '\n')
 
 while True:
-    mod = input('Would you like to add a new password or view existing ones (view, add)? Press q to quit. ').lower()
+    mod = input("Would you like to add a new password or view existing ones (view, add)? Press q to quit. ").lower()
 
     if mod == 'q':
         break
-    elif mod == 'view':
+    elif mod == 'view' or mod == 'V' or mod == 'v':
         view()
-    elif mod == 'add':
+    elif mod == 'add' or mod == 'A' or mod == 'a':
         add()
     else:
         print('Invalid mode.')
-        continue
