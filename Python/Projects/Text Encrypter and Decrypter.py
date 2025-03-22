cc1decbb72d869faf0f5042bd7764292e06d58a0
TheBandit105 <shavinminecraft@gmail.com>
2025-03-22 20:00:37 +0000

    Call main function as driver code

--- Python/Projects/Text Encrypter and Decrypter.py
+++ Python/Projects/Text Encrypter and Decrypter.py
@@ -1,27 +1,32 @@
 
-while True:
+def main():
+    
+    while True:
 
-    selector = input('Do you wish to encrypt or decrypt the word (encrypt => e or E, decrypt => d or D)? Press q or Q to quit.')
+        selector = input('Do you wish to encrypt or decrypt the word (encrypt => e or E, decrypt => d or D)? Press q or Q to quit.')
 
-    if selector == 'E' or selector == 'e':
-        wordEncrypt = input('Please type the word you would like to encrypt: ')
-        key = int(input('Input the key (0 - 25)'))
+        if selector == 'E' or selector == 'e':
+            wordEncrypt = input('Please type the word you would like to encrypt: ')
+            key = int(input('Input the key (0 - 25)'))
 
-        message_encrypted = encrypt(wordEncrypt, key)
-        print(f'Encrypted message: {message_encrypted}')
+            message_encrypted = encrypt(wordEncrypt, key)
+            print(f'Encrypted message: {message_encrypted}')
 
-    elif selector == 'D' or selector == 'd':
+        elif selector == 'D' or selector == 'd':
 
-        wordDecrypt = input('Please type the encrypted word you would like to decrypt: ')
-        key = int(input('Input the key (0 - 25), but please ensure that it is the same key that you had used to orignally encrypt the message otherwise the encrypted word wil be decrypted incorrectly: '))
+            wordDecrypt = input('Please type the encrypted word you would like to decrypt: ')
+            key = int(input('Input the key (0 - 25), but please ensure that it is the same key that you had used to orignally encrypt the message otherwise the encrypted word wil be decrypted incorrectly: '))
 
-        message_decrypted = decrypt(wordDecrypt, key)
-        print(f'Decrypted message: {message_decrypted}')
+            message_decrypted = decrypt(wordDecrypt, key)
+            print(f'Decrypted message: {message_decrypted}')
 
-    elif selector == 'Q' or selector == 'q':
-        break
+        elif selector == 'Q' or selector == 'q':
+            break
 
-    else:
-        print('Invalid mode. Please try again!')
+        else:
+            print('Invalid mode. Please try again!')
+
+if __name__ == "__main__":
+    main()
     
 
