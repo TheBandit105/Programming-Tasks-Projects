8493742c10ba0b9998bfa2b6b8dfad0d45e954df
TheBandit105 <shavinminecraft@gmail.com>
2025-03-22 18:50:17 +0000

    Added input options for Text Encrypter and Decrypter

--- Python/Projects/Text Encrypter and Decrypter.py
+++ Python/Projects/Text Encrypter and Decrypter.py
@@ -1,15 +1,21 @@
 
-selector = input('Do you wish to encrypt or decrypt the word (encrypt => e or E, decrypt => d or D)? Press q or Q to quit.')
+while True:
 
-if selector == 'E' or selector == 'e':
+    selector = input('Do you wish to encrypt or decrypt the word (encrypt => e or E, decrypt => d or D)? Press q or Q to quit.')
 
-    wordEncrypt = input('Please type the word you would like to encrypt: ')
+    if selector == 'E' or selector == 'e':
+        wordEncrypt = input('Please type the word you would like to encrypt: ')
+        key = int(input('Input the key (0 - 25)'))
 
-elif selector == 'D' or selector == 'd':
+    elif selector == 'D' or selector == 'd':
 
-    wordDecrypt = input('Please type the encrypted word you would like to decrypt: ')
+        wordDecrypt = input('Please type the encrypted word you would like to decrypt: ')
+        key = int(input('Input the key (0 - 25), but please ensure that it is the same key that you had used to orignally encrypt the message otherwise the encrypted word wil be decrypted incorrectly: '))
 
-elif selector == 'Q' or selector == 'q':
-    quit
-else:
+    elif selector == 'Q' or selector == 'q':
+        break
+    
+    else:
+        print('Invalid mode. Please try again!')
+    
 
