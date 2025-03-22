00de94b5bfd32a4066efcbfede30f5ed78939de5
TheBandit105 <shavinminecraft@gmail.com>
2025-03-22 19:57:43 +0000

    Encrypt and decrypt function called (actual functions not created yet)

--- Python/Projects/Text Encrypter and Decrypter.py
+++ Python/Projects/Text Encrypter and Decrypter.py
@@ -7,14 +7,20 @@ while True:
         wordEncrypt = input('Please type the word you would like to encrypt: ')
         key = int(input('Input the key (0 - 25)'))
 
+        message_encrypted = encrypt(wordEncrypt, key)
+        print(f'Encrypted message: {message_encrypted}')
+
     elif selector == 'D' or selector == 'd':
 
         wordDecrypt = input('Please type the encrypted word you would like to decrypt: ')
         key = int(input('Input the key (0 - 25), but please ensure that it is the same key that you had used to orignally encrypt the message otherwise the encrypted word wil be decrypted incorrectly: '))
 
+        message_decrypted = decrypt(wordDecrypt, key)
+        print(f'Decrypted message: {message_decrypted}')
+
     elif selector == 'Q' or selector == 'q':
         break
-    
+
     else:
         print('Invalid mode. Please try again!')
     
