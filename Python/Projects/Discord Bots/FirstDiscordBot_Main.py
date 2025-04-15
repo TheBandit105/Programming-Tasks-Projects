822816413a351275e00dbb73cce83afa48624a30
TheBandit105 <shavinminecraft@gmail.com>
2025-04-15 23:59:17 +0100

    Adding sending message option (in progress)

--- Python/Projects/Discord Bots/FirstDiscordBot_Main.py
+++ Python/Projects/Discord Bots/FirstDiscordBot_Main.py
@@ -1,13 +1,25 @@
-import discord
+from typing import Final
+from discord import Intents, Client, Message
 import os
 from dotenv import load_dotenv
 # from Responses import get_response
 
 load_dotenv()
 
-intents = discord.Intents.default()
+intents = Intents.default()
 intents.message_content = True 
-client = discord.Client(intents=intents)
+client = Client(intents=intents)
+
+async def send_message(message: Message, user_message: str) -> None:
+    if not user_message:
+        print('Message was empty probably due to intents being improperly or incorrectly enabled.')
+        return
+    
+    if is_private := user_message[0] == '?':
+        user_message = user_message[1:]
+        
+
+
 
 @client.event
 async def on_ready():
