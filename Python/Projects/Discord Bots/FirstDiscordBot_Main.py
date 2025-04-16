6a88b88927f0ff12bf84609722da42dcf58b66ba
TheBandit105 <shavinminecraft@gmail.com>
2025-04-16 23:59:34 +0100

    send_message async func complete

--- Python/Projects/Discord Bots/FirstDiscordBot_Main.py
+++ Python/Projects/Discord Bots/FirstDiscordBot_Main.py
@@ -2,7 +2,7 @@ from typing import Final
 from discord import Intents, Client, Message
 import os
 from dotenv import load_dotenv
-# from Responses import get_response
+from Responses import get_response
 
 load_dotenv()
 
@@ -15,11 +15,14 @@ async def send_message(message: Message, user_message: str) -> None:
         print('Message was empty probably due to intents being improperly or incorrectly enabled.')
         return
     
-    if is_private := user_message[0] == '?':
+    if is_private := user_message[0] == 'prv':
         user_message = user_message[1:]
-        
 
-
+    try:
+        response: str = get_response(user_message)
+        await message.author.send(response) if is_private else message.channel.send(response)
+    except Exception as error:
+        print("An error occurred, please check and try again.")    
 
 @client.event
 async def on_ready():
