57e9c9df630f31dba490c88b68393615405187ce
TheBandit105 <shavinminecraft@gmail.com>
2026-08-23 23:27:32 +0100

    Added initial network interface and IPv4 collection

--- Python/Projects/FieldAssist/collectors/network_info.py
+++ Python/Projects/FieldAssist/collectors/network_info.py
@@ -1,5 +1,15 @@
 import socket
+import psutil
 
-hostname = socket.gethostname()
+## hostname = socket.gethostname()
 
-print(socket.gethostbyname(hostname))
+## print(socket.gethostbyname(hostname))
+
+interfaces = psutil.net_if_addrs()
+
+for name, addresses in interfaces.items():
+	print(name)
+      
+	for address in addresses:
+            if address.family == socket.AF_INET:
+                 print(f'IPv4: {address.address}')
