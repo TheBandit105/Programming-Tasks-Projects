e421eff358b8c1fde69f16dc2fc03b904695c5b7
TheBandit105 <shavinminecraft@gmail.com>
2026-08-30 23:52:53 +0100

    Added network interface status detection

--- Python/Projects/FieldAssist/collectors/network_info.py
+++ Python/Projects/FieldAssist/collectors/network_info.py
@@ -6,10 +6,17 @@ import psutil
 ## print(socket.gethostbyname(hostname))
 
 interfaces = psutil.net_if_addrs()
+stats = psutil.net_if_stats()
 
 for name, addresses in interfaces.items():
-	print(name)
-      
-	for address in addresses:
-            if address.family == socket.AF_INET:
-                 print(f'IPv4: {address.address}')
+       print(name)
+
+       if stats[name].isup:
+          print('Status: Up')
+       else:
+          print('Status: Down') 
+
+       for address in addresses:
+          if address.family == socket.AF_INET:
+               print(f'IPv4: {address.address}')
+               
